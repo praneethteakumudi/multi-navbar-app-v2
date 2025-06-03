3faaccd7e30fd971c10f8c1ff80b1cb69bc0c4b5
Srilatha Gonuguntla <gonuguntlasrilatha20@gmail.com>
2025-06-03 13:47:59 +0530

    intial2 change

--- src/app/big-module/sub-module1/sub-module1-module.ts
+++ src/app/big-module/sub-module1/sub-module1-module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SubModule1RoutingModule } from './sub-module1-routing-module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    SubModule1RoutingModule
+    SubModule1RoutingModule,
+    RouterModule
   ]
 })
 export class SubModule1Module { }
